c900cfd33fbf152f50da3e82408d2d6280b860ab
Mark Rivers <MarkRivers@users.noreply.github.com>
2014-09-16 20:25:20 +0000

    Minor change to avoid compiler warning

--- dac128VApp/src/drvDac128V.cpp
+++ dac128VApp/src/drvDac128V.cpp
@@ -137,8 +137,7 @@ void DAC128V::report(FILE *fp, int details)
 /** Configuration command, called directly or from iocsh */
 extern "C" int initDAC128V(const char *portName, int carrier, int slot)
 {
-    DAC128V *pDAC128V = new DAC128V(portName, carrier, slot);
-    pDAC128V = NULL;  /* This is just to avoid compiler warnings */
+    new DAC128V(portName, carrier, slot);
     return(asynSuccess);
 }
 
